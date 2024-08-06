361bc3e30d7d1a246137f53bdf49904395099a7c
Bilal <bilalbilin12@gmail.com>
2024-08-06 13:42:10 +0300

    Add project files.

---
+++ PatikaWeek4PracticeConstructor/Baby.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticeConstructor
+{
+    // Creating the class
+    public class Baby
+    {
+        public string _name;
+        public string _surname;
+        public DateTime _birthday;
+
+        // Constructor which taking parameters
+        public Baby(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+            _birthday = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        // Default constructor
+        public Baby()
+        {
+            _birthday = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        // Method to print information about Baby objects.
+        public void BabyInfo()
+        {
+            Console.WriteLine($"Bebeğin Adı: {_name}\nBebeğin Soyadı: {_surname}\nDoğum Tarihi: {_birthday}");
+        }
+    }
+}
---
+++ PatikaWeek4PracticeConstructor/Program.cs
@@ -0,0 +1,23 @@
+﻿// Bebek sınıfının propertyleri -> Doğum Tarihi , Ad, Soyad 
+
+// Bebek sınıfı için 2 adet constructor tanımlayınız.
+
+// 1.si Default Constructor tarzında parametre almayan bir metot.
+
+// 2.si Ad ve Soyad parametrelerini alarak newleme işlemi sırasında değerleri propertylere atayan alternatif constructor.
+
+// 2 Constructor içinde ortak olarak bebek nesnesi oluşturulduğunda konsol ekranında bir " Ingaaaa " yazısı görülsün ve Doğum Tarihi o an olarak atansın.
+
+// Tanımlama sonrasında her iki constructor ile de birer bebek nesnesi tanımlayarak özelliklerini konsol ekranına yazdırınız.
+
+using PatikaWeek4PracticeConstructor;
+
+// Creating a Baby object using constructor which take parameters
+Baby baby1 = new Baby("Bilal", "Bilin");
+baby1.BabyInfo();
+
+// Creating a Baby object using default constructor
+Baby baby2 = new Baby();
+baby2._name = "Ciwan";
+baby2._surname = "Ali";
+baby2.BabyInfo();
